d681e79f1a74fb961bea31189b355a828054644f
Harshit <harshitkholiya1003@gmail.com>
2026-01-20 18:20:28 +0530

    Refactor housing prediction pipeline and model saving

--- housing(practice)/final.py
+++ housing(practice)/final.py
@@ -1,69 +1,72 @@
-import pandas as pd 
+import os 
 import numpy as np
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import root_mean_squared_error
-from sklearn.model_selection import cross_val_score # for avaoiding overfitting the data 
-# read the data
-df=pd.read_csv('housing.csv')
+import pandas as pd 
+import joblib 
+from sklearn.model_selection import StratifiedShuffleSplit #for train and test set
+from sklearn.preprocessing import OneHotEncoder # for encoding cat into num
+from sklearn.preprocessing import StandardScaler # for scaling the data for precise output 
+from sklearn.impute import SimpleImputer # Nan vlues
+from sklearn.pipeline import Pipeline # pipeline fast 
+from sklearn.compose import ColumnTransformer # operations on selected columns only 
+# from sklearn.linear_model import LinearRegression 
+# from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor # Ml MOdel for used for prediction 
+# from sklearn.metrics import root_mean_squared_error  # for checking the accuracy of the model 
+# from sklearn.model_selection import cross_val_score # for avaoiding overfitting the data 
 
-df['income_cat']=pd.cut(df['median_income'],bins=[0.0,1.5,3.0,4.5,6.0,np.inf],labels=[1,2,3,4,5])
-# Split the data into 2 sets test and train 
-split=StratifiedShuffleSplit(n_splits=1,test_size=0.2,random_state=43)
+MODEL_FILE='model.pkl'
+PIPELINE_FILE='pipeline.pkl'
 
-for tr,tst in split.split(df,df['income_cat']):
-    train=df.iloc[tr].drop('income_cat',axis=1)
-    test=df.iloc[tst].drop('income_cat',axis=1)
-
-data=train.copy()
-
-ylabel=data['median_house_value'].copy()
-data=data.drop('median_house_value',axis=1)
-# Sapereate the num_colmans anf cat_columns 
-num_col=data.drop('ocean_proximity',axis=1).columns.tolist()
-cat_col=['ocean_proximity']
-# Things to do in the numrerical values
-num_pipeline=Pipeline([
-                     ('imputer',SimpleImputer(strategy='median')),
-                     ('scaling',StandardScaler())
-                ])
-# things to do with the category values
-cat_pipeline=Pipeline([
-                 ('encoding',OneHotEncoder(handle_unknown='ignore'))
-                       ])
-
-# final pipeplines fit and transform 
-full_pipeline=ColumnTransformer([('num',num_pipeline,  num_col),
-                                 ('cat',cat_pipeline, cat_col)
-                                 ]) 
-final_data=full_pipeline.fit_transform(data)
-print('LinearRegression-')
-linear=LinearRegression()
-linear.fit(final_data,ylabel)
-lin_preds=linear.predict(final_data)
-linrms=-cross_val_score(linear,final_data,ylabel,scoring='neg_root_mean_squared_error',cv=5)
-print(pd.Series(linrms).describe())
-print()
+def build_pipeline(num_col,cat_col):
+    num_pipeline = Pipeline([('imputer',SimpleImputer(strategy='median')),
+                             ('scaliing',StandardScaler())
+                             ])
+    cat_pipeline=Pipeline([('Encoding',OneHotEncoder(handle_unknown='ignore'))
+                           ])
 
 
-print('DecisionTreeRegressor-')
-tree=DecisionTreeRegressor()
-tree.fit(final_data,ylabel)
-line_preds=tree.predict(final_data)
-tenrms=-cross_val_score(tree,final_data,ylabel,scoring='neg_root_mean_squared_error',cv=5)
-print(pd.Series(tenrms).describe())
-print()
+    full_pipeline= ColumnTransformer([('num',num_pipeline,num_col),
+                                      ('cat',cat_pipeline,cat_col)
+                                      ])
+    
 
-print('RandomForestRegressor-')
-randomm=RandomForestRegressor()
-randomm.fit(final_data,ylabel)
-line_=randomm.predict(final_data)
-linrmss=-cross_val_score(randomm,final_data,ylabel,scoring='neg_root_mean_squared_error',cv=5)
-print(pd.Series(linrmss).describe())
+    return full_pipeline
+
+
+if not os.path.exists(MODEL_FILE) :
+    housing=pd.read_csv(r'C:\Users\kholi\datascience\numpy\Machine learning\housing.csv')
+    housing['income_cat']=pd.cut(housing['median_income'],bins=[0.0,1.5,3.0,4.5,6.0,np.inf],labels=[1,2,3,4,5])
+    split=StratifiedShuffleSplit(n_splits=1,test_size=0.2,random_state=42)
+    for trn,tst in split.split(housing,housing['income_cat']) :
+        housingg= housing.loc[trn].drop('income_cat',axis=1)
+        housing.loc[tst].drop('income_cat',axis=1).to_csv('input.csv',index=False)
+
+    ylabel=housingg['median_house_value'].copy()
+    housingg=housingg.drop('median_house_value',axis=1)
+
+    cat_col=['ocean_proximity']
+    num_col=housingg.drop('ocean_proximity',axis=1).columns.tolist()
+
+    pipeline=build_pipeline(num_col,cat_col)
+    housing_fin=pipeline.fit_transform(housingg)
+
+
+    model=RandomForestRegressor(random_state=43)
+    model.fit(housing_fin,ylabel)
+
+    joblib.dump(model,MODEL_FILE)
+    joblib.dump(pipeline,PIPELINE_FILE)
+
+    print("Done")
+
+else :
+    model=joblib.load(MODEL_FILE)
+    pipeline=joblib.load(PIPELINE_FILE)
+
+    input_data=pd.read_csv('inputcopy.csv')
+    transformed_input=pipeline.transform(input_data) # pipeline alreday know waht to do 
+    predictions=model.predict(transformed_input) # model is already trained 
+    input_data['median_house_value']=predictions # saves the predicted values in the new column
+    input_data.to_csv('ouput.csv',index=False)
+    print("Done")
+
